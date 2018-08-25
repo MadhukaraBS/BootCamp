8dabb17121334e3938aadeaa248786b4955faf65
aakaasha <36800379+MadhukaraBS@users.noreply.github.com>
2018-08-24 20:57:02 -0700

    Ver-4

--- 07-SQL/Query_page.sql
+++ 07-SQL/Query_page.sql
@@ -161,3 +161,39 @@ show create table address;
 
 -- 6a. Use JOIN to display the first and last names, as well as the address,
 -- of each staff member. Use the tables staff and address :
+-- select * from address;
+-- select * from staff;
+select first_name, last_name, address
+from staff
+join address using (address_id);
+
+-- 6b. Use JOIN to display the total amount rung up by each staff member in August of 2005. Use
+-- tables staff and payment .
+select staff_id, first_name, last_name, sum(amount)
+from staff s
+join payment p using (staff_id)
+where DATE(payment_date) > '2005-07-31' and DATE(payment_date) < '2005-09-01'
+group by staff_id;
+
+-- select count(*) from staff;
+-- select customer_id, staff_id from payment;
+-- select customer_id, staff_id, payment_date from payment where DATE(payment_date) > '2005-07-31' and DATE(payment_date) < '2005-09-01';
+-- select staff_id, count(*) from payment group by staff_id;
+
+-- 6c. List each film and the number of actors who are listed
+-- for that film. Use tables film_actor and film . Use
+-- inner join.
+
+select title, count(*) as `Number of Actors` from film f
+inner join film_actor fa using (film_id)
+group by title;
+
+-- 6d. How many copies of the film Hunchback Impossible exist in the inventory system?
+select title, count(*) from film f
+inner join inventory i on f.film_id = i.film_id
+where f.title = 'Hunchback Impossible'
+group by title;
+
+-- 6e. Using the tables payment and customer and the
+-- JOIN command, list the total paid by each customer. List
+-- the customers alphabetically by last name:
