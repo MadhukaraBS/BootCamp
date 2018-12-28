58c53c45b22c8edfcbae36e4e2ac64da07e31a36
aakaasha <36800379+MadhukaraBS@users.noreply.github.com>
2018-12-27 23:47:48 -0800

    First version

---
+++ Project_2/finance_tech_analysis/app.py
@@ -0,0 +1,66 @@
+# import necessary libraries
+from flask import (
+    Flask,
+    render_template,
+    jsonify,
+    request)
+
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///AAPL.sqlite"
+
+db = SQLAlchemy(app)
+
+class DailyStockData(db.Model):
+    __tablename__ = 'StockPrices'
+
+    id = db.Column(db.Integer, primary_key=True)
+    Date = db.Column(db.String(64))
+    Open = db.Column(db.Float)
+    High = db.Column(db.Float)
+    Low = db.Column(db.Float)
+    Close = db.Column(db.Float)
+    Volume = db.Column(db.Integer)
+
+    def __repr__(self):
+        return '<StockDate %r>' % (self.id)
+
+
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+
+@app.route("/line")
+def test():
+    print("1")
+    results = db.session.query(
+            DailyStockData.Date,
+            DailyStockData.Open,
+            DailyStockData.High,
+            DailyStockData.Low,
+            DailyStockData.Close,
+            DailyStockData.Volume,
+            ).all()
+    print("2")
+
+    daily_data = []
+    print("3")
+    for result in results:
+        print("4")
+        print(result[0])
+        daily_data.append({
+            "Date": result[0],
+            "Open": result[1],
+            "High": result[2],
+            "Low": result[3],
+            "Close": result[4],
+            "Volume": result[5],
+        })
+    return jsonify(daily_data)
+
+if __name__ == "__main__":
+    app.run(debug=True)
